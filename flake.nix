64fa33efdd85b01b1b103a46bad3954299cea630
EcSolticia <ecsolticia@gmail.com>
2025-07-18 22:25:52 +0600

    use flake-utils for multi-system definitions

--- flake.nix
+++ flake.nix
@@ -4,37 +4,26 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, ... }: let
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-
-    packages = forAllSystems (system: let
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs {
         inherit system overlays;
       };
       rsTools = pkgs.rust-bin.stable."1.88.0".default;
-      textmachinePkg = pkgs.callPackage ./. {
+    in {
+
+      packages.default = pkgs.callPackage ./. {
         inherit rsTools;
       };
-    in {
-      default = textmachinePkg;
-    });
 
-    devShells = forAllSystems (system: let
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
+      devShells.default = pkgs.mkShell {
+        packages = [ rsTools pkgs.bash ];
       };
-      rsTools = pkgs.rust-bin.stable."1.88.0".default;
-    in {
-      default = pkgs.mkShell {
-        packages = [rsTools pkgs.bash];
-      };
+
     });
-    
-  };
+  
 }
