c263ec2f97a2bfb33192a084d8d8e4054eddd92a
EcSolticia <ecsolticia@gmail.com>
2025-07-18 19:47:54 +0600

    create initial flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "textmachine flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, ... }: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+
+    devShells = forAllSystems (system: let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      rsTools = pkgs.rust-bin.stable."1.88.0".default;
+    in {
+      default = pkgs.mkShell {
+        packages = [rsTools pkgs.bash];
+      };
+    });
+    
+  };
+}
