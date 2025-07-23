5e28dd94b32a961b140d2b9c767badbc4812db50
EcSolticia <ecsolticia@gmail.com>
2025-07-24 01:14:29 +0600

    add pandoc lua filter turning .md links into .html links

--- resources/filters.lua
+++ resources/filters.lua
@@ -10,3 +10,24 @@ function Header (h)
     return h
   end
 end
+
+function strip_end(content, endw)
+  local nc = #content
+  local ne = #endw
+
+  if ne <= nc and string.sub(content, -ne) == endw then
+    return string.sub(content, 1, nc - ne)
+  else
+    return content
+  end
+end
+
+function md_to_html_link(link_target)
+  local tm = strip_end(link_target, ".md")
+  return tm .. ".html"
+end
+
+function Link(el)
+  local new_target = md_to_html_link(el.target)
+  return pandoc.Link(el.content, new_target, el.title)
+end
--- test-inputs/main.textmachine.page.md
+++ test-inputs/main.textmachine.page.md
@@ -1,3 +1,7 @@
 # Hi, I am the main page!
 
 Main page of what, you ask? I have no idea...
+
+[Wanna go to p1? Haah](./p1/p1.textmachine.page.md)
+[Wanna go to p2? Haah](./p2/p2.textmachine.page.md)
+[Wanna go to p3? Haah](./p3/p3.textmachine.page.md)
