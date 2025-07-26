84452389f5a52ae40f6f0eea2124b556efe059a2
EcSolticia <ecsolticia@gmail.com>
2025-07-26 13:12:12 +0600

    implement anchored link target conversion in lua filter

--- resources/filters.lua
+++ resources/filters.lua
@@ -11,31 +11,13 @@ function Header (h)
   end
 end
 
-function strip_end(content, endw)
-  local nc = #content
-  local ne = #endw
-
-  if ne <= nc and string.sub(content, -ne) == endw then
-    return string.sub(content, 1, nc - ne)
-  else
-    return content
-  end
-end
-
-function md_to_html_link(link_target)
-  local tm1 = strip_end(link_target, ".md")
-  local tm2 = strip_end(link_target, ".md/")
-
-  local tm = ""
-
-  if tm1 ~= link_target then tm = tm1
-  elseif tm2 ~= link_target then tm = tm2
-  else return link_target end
-
-  return tm .. ".html"
+function link_md_to_html(target)
+  return target:gsub("%.md([#?])", ".html%1")
+               :gsub("%.md$", ".html")
 end
 
 function Link(el)
-  local new_target = md_to_html_link(el.target)
+  local new_target = link_md_to_html(el.target)
+  
   return pandoc.Link(el.content, new_target, el.title)
 end
