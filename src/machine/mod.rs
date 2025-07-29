bd0cdeecb7d1e846f916248b80c771c1df989eaa
EcSolticia <ecsolticia@gmail.com>
2025-07-30 04:47:30 +0600

    delete output dir pre-generation

--- src/machine/mod.rs
+++ src/machine/mod.rs
@@ -11,7 +11,9 @@ pub struct CmdArgs {
     #[arg(short, long)]    
     pub input_path: PathBuf,
     #[arg(short, long)]
-    pub output_path: PathBuf
+    pub output_path: PathBuf,
+    #[arg(short, long)]
+    pub force: bool
 }
 impl CmdArgs {
     fn mirror_input_path(&self, path: PathBuf) -> PathBuf {
@@ -32,10 +34,72 @@ impl CmdArgs {
     }
 }
 
+pub mod sure_prompt {
+    fn print_line(line: &str) {
+        println!("[textmachine-sure_prompt] {}", line);
+    }
+    
+    pub fn handle(args: &super::CmdArgs) -> Option<bool> {        
+
+        if args.force {
+            return Some(true);
+        }
+
+        print_line("----");
+        print_line("Called textmachine with the following arguments:");
+        print_line(
+            format!("input_path: {}", args.input_path.to_str().unwrap()).as_str()
+        );
+        print_line(
+            format!("output_path: {}", args.output_path.to_str().unwrap()).as_str()  
+        );
+        print!("\n");
+        print_line("!!!IMPORTANT!!!");
+        print_line("textmachine will delete the output path directory prior to generating pages");
+        print_line("Before continuing, ensure that the output path is really disposable.");
+        print_line("Ideally, in a git repository, it should be gitignored.");
+        print_line("Please confirm that you are certain, that outpath_path, if it exists, is disposable. [y/N]");
+        print!("\n");
+
+        let mut answer: String = String::new();
+        
+        match std::io::stdin().read_line(&mut answer) {
+            Ok(_) => {
+                let trimmed_answer = answer.trim();
+                match trimmed_answer {
+                    "n" | "N" | "no" | "No" => return Some(false),
+                    "y" | "Y" | "yes" | "Yes" => return Some(true),
+                    _ => {
+                        print_line("Answer is not 'y' or 'N'. Assuming 'N'.");
+                        return Some(false);
+                    }
+                }
+            },
+            Err(_) => return None
+        };
+    }
+}
+
 pub fn execute(args: CmdArgs) {
     match input::TracedPages::trace_pages(&args.input_path) {
         Ok(traced_pages) => {
             let output_pages: output::OutputPages = output::OutputPages::new(traced_pages, &args);
+
+            
+            if let Some(sure) = sure_prompt::handle(&args) {
+                if !sure {
+                    return;
+                }
+            } else {
+                println!("Could not read line from stdin, returning.");
+                return;
+            }
+            
+            match std::fs::remove_dir_all(args.output_path) {
+                Ok(_) => (),
+                Err(e) => println!("removing output dir: {:#?}", e)
+            }
+
             let gen_output = generator::generate(output_pages);
 
             println!("gen_output: {:#?}", gen_output);
